87938d232920081a472d5d79e888d1e91d411f9d
pryscila dinda <pryscila.dinda06@gmail.com>
2023-07-20 21:38:38 +0700

    update for deploy

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 const name = "Pryscila Dinda";
 export const siteTitle = "Next.js Sample Website";
 
-export default function Layout({ children, home }) {
+export default function layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
